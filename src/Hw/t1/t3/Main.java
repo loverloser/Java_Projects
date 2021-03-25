e36f4b23a17b3ee9c19cae9eca424fc10ffa064b
lover <timofeilobanovich@gmail.com>
2021-03-25 15:57:52 +0300

    all task complete

--- src/Hw/t1/t3/Main.java
+++ src/Hw/t1/t3/Main.java
@@ -1,59 +1,71 @@
 package Hw.t1.t3;
 
-public class Main {
-    private static final Object lock = new Object();
-    public static void main(String[] args) {
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                    synchronized (lock) {
-                        for (int i = 0; i < 5; i++) {
-                            System.out.print("A");
-                            lock.notifyAll();
-                            try {
-                                lock.wait();
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-            }
-        });
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                synchronized (lock) {
-                    for (int i = 0; i < 5; i++) {
-                        System.out.print("B");
-                        lock.notifyAll();
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-        });
-        Thread t3 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                synchronized (lock) {
-                    for (int i = 0; i < 5; i++) {
-                        System.out.print("C");
-                        lock.notifyAll();
-                        try {
-                            lock.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-        });
-            t1.start();
-            t2.start();
-            t3.start();
+import jdk.swing.interop.SwingInterOpUtils;
 
+public class Main {
+    private static final Object LOCK = new Object();
+    private static final String A = "A";
+    private static final String B = "B";
+    private static final String C = "C";
+    private static  String nextLetter = A;
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (LOCK){
+                    for (int i = 0; i < 5; i++){
+                        try {
+                            while (!nextLetter.equals(A)){
+                                LOCK.wait();
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.print(A);
+                        nextLetter = B;
+                        LOCK.notifyAll();
+                    }
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (LOCK){
+                    for (int i = 0; i < 5; i++){
+                        try {
+                            while (!nextLetter.equals(B)){
+                                LOCK.wait();
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.print(B);
+                        nextLetter = C;
+                        LOCK.notifyAll();
+                    }
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (LOCK){
+                    for (int i = 0; i < 5; i++){
+                        try {
+                            while (!nextLetter.equals(C)){
+                                LOCK.wait();
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.print(C);
+                        nextLetter = A;
+                        LOCK.notifyAll();
+                    }
+                }
+            }
+        }).start();
     }
 }
