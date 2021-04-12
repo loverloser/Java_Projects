ffba06b0d964f4af4332a4c96a75f5c00e3728f5
Lover loser <65848887+loverloser@users.noreply.github.com>
2021-04-12 11:37:13 +0300

    cum

--- src/main/java/labs/t9/Main.java
+++ src/main/java/labs/t9/Main.java
@@ -4,11 +4,11 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        firstTask(1,2,3,4);
+//        firstTask(1,2,3,4);
         System.out.println(secondTask("абвгд вфыв пар р ошгш гш нз щш"));
     }
 
-    public static String firstTask(int a, int b, int a1, int b1){
+    public  String firstTask(int a, int b, int a1, int b1){
         double firstG = Math.sqrt(Math.pow(a, 2) + Math.pow(b,2));
         double secondG = Math.sqrt(Math.pow(a1, 2) + Math.pow(b1,2));
         if(firstG > secondG){
--- src/test/java/labs/t9/MainTest.java
+++ src/test/java/labs/t9/MainTest.java
@@ -5,6 +5,8 @@ import junit.framework.TestCase;
 public class MainTest extends TestCase {
 
     public void testFirstTask() {
-        assertEquals("Гиппотенуза 2 - больше", "[eq");
+        Main main = new Main();
+        String result = main.firstTask(1,2,3,4);
+        assertEquals("Гиппотенуза 2 - больше", result);
     }
 }
