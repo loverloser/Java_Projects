257ed41f87e1c7aec5cc439da1aef14aee4032d7
Lover loser <65848887+loverloser@users.noreply.github.com>
2021-06-19 00:02:11 +0300

    complete

---
+++ src/main/java/com/labanovich/dmdev/t4/Car/Garage.java
@@ -0,0 +1,39 @@
+package com.labanovich.dmdev.t4.Car;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Garage {
+    private Map<Car, Integer> map = new HashMap<>();
+
+    public void parking(Car car){
+        if(!map.containsKey(car)){
+            map.put(car, 1);
+        }else {
+            for (Map.Entry<Car, Integer> entry : map.entrySet()) {
+                if(entry.getKey().equals(car)){
+                    map.put(car, entry.getValue() + 1);
+                }
+            }
+        }
+    }
+
+    public void out(Car car){
+        if(map.containsKey(car)){
+            for (Map.Entry<Car, Integer> entry : map.entrySet()) {
+                if(entry.getKey().equals(car)){
+                    map.put(car, entry.getValue() - 1);
+                }
+            }
+        } else System.out.println("There isn't auto in garage");
+    }
+
+    public int how(Car car){
+        for (Map.Entry<Car, Integer> entry : map.entrySet()) {
+            if(entry.getKey().equals(car)){
+                return entry.getValue();
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/main/java/com/labanovich/dmdev/t4/Car/Tesla.java
@@ -0,0 +1,62 @@
+package com.labanovich.dmdev.t4.Car;
+
+public class Tesla extends Car{
+    public Tesla(String mark, String model, int year, String color) {
+        super(mark, model, year, color);
+    }
+
+    @Override
+    public String getMark() {
+        return super.getMark();
+    }
+
+    @Override
+    public void setMark(String mark) {
+        super.setMark(mark);
+    }
+
+    @Override
+    public String getModel() {
+        return super.getModel();
+    }
+
+    @Override
+    public void setModel(String model) {
+        super.setModel(model);
+    }
+
+    @Override
+    public int getYear() {
+        return super.getYear();
+    }
+
+    @Override
+    public void setYear(int year) {
+        super.setYear(year);
+    }
+
+    @Override
+    public String getColor() {
+        return super.getColor();
+    }
+
+    @Override
+    public void setColor(String color) {
+        super.setColor(color);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
